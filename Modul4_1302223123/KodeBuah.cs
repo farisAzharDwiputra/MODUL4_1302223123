511c81cdcdb852221e23add288e11918ae3e0c45
Faris Azhar Dwiputra <farisazhar.0241@gmail.com>
2024-03-13 13:11:00 +0700

    Menambahkan class KodeBuah

--- Modul4_1302223123/KodeBuah.cs
+++ Modul4_1302223123/KodeBuah.cs
@@ -10,7 +10,15 @@ public class KodeBuah
         {"Pisang", "D00" },
         { "Paprika","E00"},
         {"Kurma","K00" },
-       
+        {"Durian","L00" },
+        {"Anggur", "M00" },
+        {"Melon", " N00" },
+        {"Semangka", "O00" },
+        {"Blackberry","F00" },
+        {"Ceri","H00 " },
+        {"Kelapa", "I00" },
+        {"Jagung", " j00" }
+
 
     };
     
