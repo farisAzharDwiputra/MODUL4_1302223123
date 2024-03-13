d2a5de314ca31750ce3e09961169ce4468a21d18
Faris Azhar Dwiputra <farisazhar.0241@gmail.com>
2024-03-13 13:08:53 +0700

    inisiasi Class KodeBuah Pertama

---
+++ Modul4_1302223123/KodeBuah.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class KodeBuah
+{
+    private Dictionary<string, string> DataBuah = new Dictionary<string, string>()
+    {
+        {"Apel","A00"},
+        {"Aprikot", "B00" },
+        {"Alpukat", "C00" },
+        {"Pisang", "D00" },
+        { "Paprika","E00"},
+        {"Kurma","K00" },
+       
+
+    };
+    
+    public string GetKodeBuah(string NamaBuah)
+    {
+        return DataBuah.ContainsKey(NamaBuah) ? DataBuah[NamaBuah] : "Data Buah Tidak Ditemukan";
+    }
+}
---
+++ Modul4_1302223123/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class program
+{
+    static void Main()
+    {
+        KodeBuah variableBuah = new KodeBuah();
+        Console.Write("Masukan Nama buah Yang kamu cari : ");
+        string inputbuah = Console.ReadLine();
+        Console.WriteLine(variableBuah.GetKodeBuah(inputbuah));
+    }
+}
